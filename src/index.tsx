19ad14fc0609aa1659ea5eab82fe8beda497c831
clockwrks <123266805+clockwrks@users.noreply.github.com>
2025-09-06 18:21:58 -0400

    asd12asd

--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,31 @@
-import manifest from '../manifest.json';
-import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React, Dispatcher } from 'enmity/metro/common';
-import { bulk, filters } from 'enmity/metro';
-import Settings from './components/Settings';
-import { Data } from 'enmity/managers';
+import { findByProps, findByStore } from "@enmity-mod/api";
 
-const [UserStore] = bulk(
-  filters.byProps('getCurrentUser', 'getUser')
-);
+const GuildStore = findByStore("GuildStore");
+const UserStore = findByStore("UserStore");
 
-let usernamePatch: any;
-let currentSettings = {
-  active: true,
-  newUsername: ''
-};
+const UserProfileActions = findByProps("updateUserProfile", "updateProfile");
+const NicknameActions = findByProps("changeNickname");
 
-const LocalUsernameChanger: Plugin = {
-  ...manifest,
+export const onLoad = () => {
+  const currentUserId = UserStore.getCurrentUser().id;
+  const currentGuildId = GuildStore.getGuildId();
 
-  onStart() {
-    // Load settings
-    const saved = Data.load('LocalUsernameChanger', 'settings') || {};
-    currentSettings = { ...currentSettings, ...saved };
-
-    // Patch getCurrentUser
-    usernamePatch = UserStore.getCurrentUser;
-    UserStore.getCurrentUser = (...args: any[]) => {
-      const res = usernamePatch.apply(UserStore, args);
-      if (currentSettings.active && currentSettings.newUsername) {
-        return { ...res, username: currentSettings.newUsername };
-      }
-      return res;
-    };
-
-    // Patch getUser for current user
-    usernamePatch = UserStore.getUser;
-    UserStore.getUser = (...args: any[]) => {
-      const res = usernamePatch.apply(UserStore, args);
-      if (currentSettings.active && currentSettings.newUsername &&
-          args[0] === UserStore.getCurrentUser()?.id) {
-        return { ...res, username: currentSettings.newUsername };
-      }
-      return res;
-    };
-
-    console.log('[LocalUsernameChanger] Plugin started.');
-  },
-
-  onStop() {
-    if (usernamePatch) {
-      UserStore.getCurrentUser = usernamePatch;
-      UserStore.getUser = usernamePatch;
-    }
-    console.log('[LocalUsernameChanger] Plugin stopped.');
-  },
-
-  getSettingsPanel({ settings }) {
-    return <Settings settings={settings} />;
+  if (currentUserId && currentGuildId) {
+    NicknameActions.changeNickname(
+      currentGuildId,
+      currentUserId,
+      'works'
+    )
+     .then(() => {
+        console.log("Nickname successfully changed to 'works'.");
+      })
+     .catch((error) => {
+        console.error("Failed to change nickname:", error);
+      });
   }
 };
 
-registerPlugin(LocalUsernameChanger);
+export const onUnload = () => {
+  // No cleanup is necessary for this simple plugin.
+  console.log("The 'works' plugin has been unloaded.");
+};
